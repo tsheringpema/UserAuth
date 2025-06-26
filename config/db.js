e45036129672fa09283101906b2ec961b795c42b
tsheringpema <tpems2019@gmail.com>
2025-06-26 10:30:07 +0600

    pull

--- config/db.js
+++ config/db.js
@@ -6,12 +6,15 @@ const pgp = pgpInit();
 
 
 const db = pgp({
-    host: 'localhost',
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    port: 5432, // Default PostgreSQL port
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    ssl: false,
+    ssl:{
+        rejectUnauthorized: false // use this only if you are sure about your database connection security
+    }
+    
 })
 
 module.exports = db;
